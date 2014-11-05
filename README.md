4f998898825a81fe4dcb853db70a31a40cb287ca
darshangandhi <darshan.gandhi@synerzip.com>
2014-11-05 16:36:54 +0530

    Test build on commit

--- README.md
+++
@@ -1,4 +0,0 @@
-jenkins
-=======
-
-Test Jenkins Setup
--- spring-mongo/spring-mongo/src/main/webapp/resources/js/common.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	boot();
-});
-
-var boot = function() {
-	console.log('Why the fuck it does not work!');
-0;}
